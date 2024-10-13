00ea4464ac239021af1e17dc141cbb263ef86f31
Farokh Madah <fa.madah@gmail.com>
2024-10-13 15:11:34 +0330

    storage contract, only for test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config'
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+  solidity: "0.8.24",
+  defaultNetwork: process.env.DEFAULT_NETWORK,
+  networks: {
+    bob: {
+      url: "https://rpc.gobob.xyz/",
+      accounts: [process.env.SERVICE_ADMIN_PRIVATE_KEY || ""]
+    },
+    bobtestnet: {
+      // url: "https://bob-sepolia.rpc.gobob.xyz",
+      url: "https://testnet.rpc.gobob.xyz/",
+      accounts: [process.env.SERVICE_ADMIN_PRIVATE_KEY || ""]
+    }
+  },
 };
 
 export default config;
